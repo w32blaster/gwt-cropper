ce293d9e5df9163314b41884f9cd4028f40a1c4d
ilja hamalainen <ilja.hamalainen@gmail.com>
2012-10-16 23:39:50 +0300

    Initial commit

---
+++ src/main/java/com/google/code/gwt/crop/client/GWTCropper.java
@@ -0,0 +1,15 @@
+package com.google.code.gwt.crop.client;
+
+import com.google.gwt.user.client.ui.HTMLPanel;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GWTCropper extends HTMLPanel {
+
+  public GWTCropper(String strImageURL) {
+		super("<div>Here will be the cropper</div>");
+	}
+
+}
