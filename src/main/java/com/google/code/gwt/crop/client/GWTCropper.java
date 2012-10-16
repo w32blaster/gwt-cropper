87f33500f00b41a4712a34a20f3ad1835fdb3fe7
ilja hamalainen <ilja.hamalainen@gmail.com>
2012-10-16 23:52:37 +0300

    Added comment

--- src/main/java/com/google/code/gwt/crop/client/GWTCropper.java
+++ src/main/java/com/google/code/gwt/crop/client/GWTCropper.java
@@ -2,14 +2,17 @@ package com.google.code.gwt.crop.client;
 
 import com.google.gwt.user.client.ui.HTMLPanel;
 
-
 /**
- * Entry point classes define <code>onModuleLoad()</code>.
+ * GWT Cropper - widget allowing to select any area on the top of a picture 
+ * in order to crop it.
+ * 
+ * @author ilja
+ *
  */
 public class GWTCropper extends HTMLPanel {
 
   public GWTCropper(String strImageURL) {
-		super("<div>Here will be the cropper</div>");
+		super("<div>Here will be the cropper. </div>");
 	}
 
 }
