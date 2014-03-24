39631df102f4852a17553af1fc85e18331fa9cc2
Ilja H?m?l?inen <ilja.hamalainen@gmail.com>
2014-03-24 10:35:09 +0000

    Merge with default

---
+++ src/main/java/com/google/code/gwt/crop/client/Constants.java
@@ -0,0 +1,18 @@
+package com.google.code.gwt.crop.client;
+
+/**
+ * Constants for the GWT cropper to match action type
+ * 
+ * @author ilja.hamalainen@gmail.com (Ilja Hämäläinen)
+ *
+ */
+interface Constants {
+
+	byte DRAG_NONE = 0;
+	byte DRAG_BACKGROUND = 1;
+	byte DRAG_TOP_LEFT_CORNER = 2;
+	byte DRAG_TOP_RIGHT_CORNER = 3;
+	byte DRAG_BOTTOM_LEFT_CORNER = 4;
+	byte DRAG_BOTTOM_RIGHT_CORNER = 5;
+
+}
