d101895aad6344026cdfdf5ec25e3ebc6388d12f
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2015-08-11 22:59:17 +0100

    update javadocs

---
+++ target/classes/com/google/code/gwt/crop/client/StylesDesktopImpl.java
@@ -0,0 +1,29 @@
+package com.google.code.gwt.crop.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+
+/**
+ * Implementation of the style bundle initializer for desktop
+ * 
+ * @author ilja.hamalainen@gmail.com (Ilja Hämäläinen)
+ *
+ */
+class StylesDesktopImpl implements ICropperStyleSource {
+
+	interface IStyleDesktop extends ClientBundle {
+		
+		@Source("GWTCropper.css")
+		CropperStyleResource getStyles();
+	}
+
+	private final IStyleDesktop bundleResources = GWT.create(IStyleDesktop.class);
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public CropperStyleResource css() {
+		return bundleResources.getStyles();
+	}
+
+}
