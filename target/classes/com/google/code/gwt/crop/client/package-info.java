bf5a8d7916fa948c553e6dd5b43827a0b9480414
Ilja Hämäläinen <ilja@p-i.net>
2015-08-12 08:51:02 +0100

    Remove target folder

--- target/classes/com/google/code/gwt/crop/client/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <p>The package contains set of Style implementations. All these classes have default
- * visibility level. It is done in order to hide these classes away of the library users.</p>
- * 
- * <p>Only {@link com.google.code.gwt.crop.client.GWTCropper GWTCropper.java} is public and visible outside.</p>
- */
-package com.google.code.gwt.crop.client;
